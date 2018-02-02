0ab90033edfc8d067d8afb693285430662ac78e0
E. Weilow <eweilow@digitalkoppar.se>
2018-02-02 16:58:26 +0100

    Add Sentry logging of errors
    Resolves #32

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import "./style";
 import App from "./components/app";
 
+import Raven from "raven-js";
+Raven
+  .config("https://e20ef790f5704cde9f95f731be7fd847@sentry.io/153019")
+  .install();
+
 export default App;
