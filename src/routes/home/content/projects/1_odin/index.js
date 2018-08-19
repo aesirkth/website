272c3b82286adac89e1cff637cd60ac67c768fdc
stovatt <sarah.tovatt@gmail.com>
2018-08-19 19:26:02 +0200

    Update text content
    Update: "We are currently working towards a late 2017 launch of Odin, a tiny yet powerful rocket."
    to: "We are currently preparing for the first launch of Odin in late October 2018, follow us on Instagram or Facebook for more frequent updates!"

--- src/routes/home/content/projects/1_odin/index.js
+++ src/routes/home/content/projects/1_odin/index.js
@@ -5,7 +5,7 @@ import urlFinsImage from "./fins.jpg";
 export default {
   title: "Project Odin",
   content: `
-  We are currently working towards a late 2017 launch of Odin, a tiny yet powerful rocket.
+  We are currently preparing for the first launch of Odin in late October 2018, follow us on Instagram or Facebook for more frequent updates!
 
   Odin will be our first sounding rocket, with a thrust of about 3kN. Just like the norse god Odin gave an eye for wisdom, our rocket Odin will give us valuable knowledge on constructing rockets.
   So far, we have conducted two static firings of our rocket motor and are preparing a third test. 
