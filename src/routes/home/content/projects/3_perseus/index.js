3b272f60504a1e8339558cb0bb299da2c0a6311b
E. Weilow <eweilow@digitalkoppar.se>
2018-09-30 12:33:24 -0600

    Update PERSEUS text

--- src/routes/home/content/projects/3_perseus/index.js
+++ src/routes/home/content/projects/3_perseus/index.js
@@ -1,6 +1,12 @@
 export default {
-  title: "PERSEUS Student Experiment",
+  title: "International collaborations",
   content: `
-  We are also in the initial planning phase for a student payload as part of the PERSEUS Project within Europe.
+  # PERSEUS
+  During fall 2017/spring 2018 some of our members participated in a collaboration with the the French government space agency CNES as a part of their PERSEUS project.
+  The project was to construct an experiment to fly on their rocket.
+  Our team decided to build a device that would measure the vibrations of the rocket.
+
+  The project was finished on time and the whole team later participated in a seminar in Paris. 
+  Unfortunatly the launch planned to be at Esrange in Kiruna, Sweden, was postponed until further notice due to problems with the rocket.
   `
 };
