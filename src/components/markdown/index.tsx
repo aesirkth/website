463db02269edb3b8625d0aa53f5d334f87ea6414
E. Weilow <eweilow@digitalkoppar.se>
2019-03-15 20:25:54 +0100

    Nicer markdown header for "full" pages

--- src/components/markdown/index.tsx
+++ src/components/markdown/index.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 import styles from "./style.css";
 import "!style-loader!css-loader!prismjs/themes/prism-tomorrow.css";
+import clsx from "clsx";
+
+const MarkdownHeader: React.FC<{
+  leftAlign: boolean;
+  prefix?: string;
+  title: string;
+  subTitle?: string;
+  readingTime?: {
+    text: string;
+    minutes: number;
+    time: number;
+    words: number;
+  };
+}> = props => (
+  <header className={clsx(styles.header, { [styles.leftAlign]: props.leftAlign })}>
+    <div className={styles.box}>
+      <div className={styles.title}>
+        {props.prefix && <span className={styles.prefix}>{props.prefix}</span>}
+        {props.title}
+      </div>
+      {props.subTitle && <div className={styles.subTitle}>{props.subTitle}</div>}
+      {props.readingTime && <div className={styles.readingTime}>{props.readingTime.text}</div>}
+    </div>
+  </header>
+);
 
 export const MarkdownWrapper: React.FC<{
+  leftAlign?: boolean;
   id?: string;
+  prefix?: string;
   title?: string;
+  subTitle?: string;
   readingTime?: {
     text: string;
     minutes: number;
@@ -13,8 +41,18 @@ export const MarkdownWrapper: React.FC<{
   };
 }> = props => (
   <section id={props.id} className={styles.markdown}>
-    {props.title && <header className={styles.header}>{props.title}</header>}
-    {props.readingTime && <p className={styles.readingTime}>{props.readingTime.text}</p>}
+    {props.title && (
+      <>
+        <MarkdownHeader
+          prefix={props.prefix}
+          leftAlign={props.leftAlign || false}
+          title={props.title}
+          subTitle={props.subTitle}
+          readingTime={props.readingTime}
+        />
+        {!props.leftAlign && <hr />}
+      </>
+    )}
     {props.children}
   </section>
 );
