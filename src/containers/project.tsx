066ae3750a031b70ee21b90164e9954bb1866c9c
E. Weilow <eweilow@digitalkoppar.se>
2019-03-15 19:20:25 +0100

    Project styling

--- src/containers/project.tsx
+++ src/containers/project.tsx
@@ -2,30 +2,31 @@ import React from "react";
 import { withRouteData } from "react-static";
 import { Project } from "../types";
 import { Column } from "@components/column";
+import { MarkdownWrapper } from "@components/markdown";
 
 export default withRouteData(({ project }: { project: Project }) => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const { default: Route, ...route } = require("@data/projects/" + project.details.fileInfo.path);
-  console.log(route);
+  const { default: Route } = require("@data/projects/" + project.details.fileInfo.path);
   return (
     <Column>
-      <h1>{project.summary.title}</h1>
-      <h2>
-        {project.summary.completed === false && (
-          <>An ongoing project since {project.summary.started}</>
-        )}
-        {project.summary.completed !== false &&
-          project.summary.completed === project.summary.started && (
-            <>A project during {project.summary.started}</>
+      <MarkdownWrapper title={project.summary.title}>
+        <h2>
+          {project.summary.completed === false && (
+            <>An ongoing project since {project.summary.started}</>
           )}
-        {project.summary.completed !== false &&
-          project.summary.completed !== project.summary.started && (
-            <>
-              A project between {project.summary.started} and {project.summary.completed}
-            </>
-          )}
-      </h2>
-      <Route />
+          {project.summary.completed !== false &&
+            project.summary.completed === project.summary.started && (
+              <>A project during {project.summary.started}</>
+            )}
+          {project.summary.completed !== false &&
+            project.summary.completed !== project.summary.started && (
+              <>
+                A project between {project.summary.started} and {project.summary.completed}
+              </>
+            )}
+        </h2>
+        <Route />
+      </MarkdownWrapper>
     </Column>
   );
 });
